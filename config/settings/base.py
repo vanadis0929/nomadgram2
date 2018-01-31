cd7247cfedda8ecf3e3b792e4a0f03fb95a2d269
rdm-surface <vanadis0929@gmail.com>
2018-02-01 00:02:41 +0900

    서버세팅

--- config/settings/base.py
+++ config/settings/base.py
@@ -111,7 +111,7 @@ MANAGERS = ADMINS
 # Uses django-environ to accept uri format
 # See: https://django-environ.readthedocs.io/en/latest/#supported-types
 DATABASES = {
-    'default': env.db('DATABASE_URL', default='postgres://localhost/nomadgram2'),
+ 'default': env.db('DATABASE_URL', default='postgres://postgres:1234@localhost:5432/nomadgram2'),
 }
 DATABASES['default']['ATOMIC_REQUESTS'] = True
 
