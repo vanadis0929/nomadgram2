3a491106a6e1b066647fce7776b1a2a92f3f9357
rdm-surface <vanadis0929@gmail.com>
2018-02-08 16:50:58 +0900

    모델 만드는중 (1-19~)

--- nomadgram2/users/models.py
+++ nomadgram2/users/models.py
@@ -8,9 +8,21 @@ from django.utils.translation import ugettext_lazy as _
 @python_2_unicode_compatible
 class User(AbstractUser):
 
+    #성별관련 상수 선언
+    GENDER_CHOICES = {
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('not-specified', 'Not specified')
+    }
     # First Name and Last Name do not cover name patterns
     # around the globe.
+    # 생성전에 추가한 값들을 기본값(공백) 으로 남기려면 null=True
+    
     name = models.CharField(_('Name of User'), blank=True, max_length=255)
+    website = models.URLField(null=True)
+    bio = models.TextField(null=True)
+    phone = models.CharField(max_length=120,null=True)
+    gender = models.CharField(max_length=80, choices=GENDER_CHOICES, null=True)
 
     def __str__(self):
         return self.username
