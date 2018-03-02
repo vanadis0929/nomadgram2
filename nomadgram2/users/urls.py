11202663da53d9921b72c32bff232a6771f33e42
rdm-surface <vanadis0929@gmail.com>
2018-03-02 22:57:41 +0900

    1-75 백앤드 완료...

--- nomadgram2/users/urls.py
+++ nomadgram2/users/urls.py
@@ -43,7 +43,11 @@ urlpatterns = [
         view=views.ChangePassword.as_view(),
         name='change'
     ),
-    
+    url(
+        regex=r'^login/facebook/$',
+        view=views.FacebookLogin.as_view(),
+        name='fb_login'
+    ),
 
 
 ]
