7dfd514d3011b0468024b86bea583a9db0d63e80
rdm-surface <vanadis0929@gmail.com>
2018-03-10 11:48:59 +0900

    frontend 앱 작성 후 eject 하기 전 시점

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,16 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import styles from "./App.scss";
-
-fetch("/notifications/");
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className={styles.App}>
-        <header className={styles.App__header}>
-          <img src={logo} className={styles.App__logo} alt="logo" />
-          <h1 className={styles.App__title}>Welcome to React</h1>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className={styles.App__intro}>
+        <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
