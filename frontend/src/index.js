cbfd82bffcf9b659ab33b81ef80e79b58a10917e
rdm-surface <vanadis0929@gmail.com>
2018-03-16 23:31:12 +0900

    3-15 완료

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,13 +6,17 @@ import store, { history } from "redux/configureStore";
 import "./index.css";
 import App from "App";
 import "ReactotronConfig";
+import I18n from "redux-i18n";
+import { translations } from "translations";
 
 //store.dispatch({ type: "HELLO" });
 
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <App />
+      <I18n translations={translations} initialLang="ko" fallbackLang="en">
+        <App />
+      </I18n>
     </ConnectedRouter>
   </Provider>,
   document.getElementById("root")
--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,9 +1,21 @@
-import { combineReducers, createStore, applyMiddleware } from "redux";
+import {
+  combineReducers,
+  createStore,
+  applyMiddleware
+} from "redux";
 import thunk from "redux-thunk";
-import { routerReducer, routerMiddleware } from "react-router-redux";
+import {
+  routerReducer,
+  routerMiddleware
+} from "react-router-redux";
 import createHistory from "history/createBrowserHistory";
-import { composeWithDevTools } from "redux-devtools-extension";
+import {
+  composeWithDevTools
+} from "redux-devtools-extension";
 import users from "redux/modules/users";
+import {
+  i18nState
+} from "redux-i18n";
 import Reactotron from "ReactotronConfig";
 
 const env = process.env.NODE_ENV;
@@ -12,13 +24,16 @@ const history = createHistory();
 const middlewares = [thunk, routerMiddleware(history)];
 
 if (env === "development") {
-  const { logger } = require("redux-logger");
+  const {
+    logger
+  } = require("redux-logger");
   middlewares.push(logger);
 }
 
 const reducer = combineReducers({
   users,
-  routing: routerReducer
+  routing: routerReducer,
+  i18nState
 });
 
 let store;
@@ -33,5 +48,7 @@ if (env === "development") {
   store = initialState => createStore(reducer, applyMiddleware(...middlewares));
 }
 
-export { history };
+export {
+  history
+};
 export default store();
