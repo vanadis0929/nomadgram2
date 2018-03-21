b70dc2d11f74aca514d0287d664f57fb8043e029
rdm-surface <vanadis0929@gmail.com>
2018-03-22 01:03:33 +0900

    3-22까지

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,21 +1,10 @@
-import {
-  combineReducers,
-  createStore,
-  applyMiddleware
-} from "redux";
+import { combineReducers, createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import {
-  routerReducer,
-  routerMiddleware
-} from "react-router-redux";
+import { routerReducer, routerMiddleware } from "react-router-redux";
 import createHistory from "history/createBrowserHistory";
-import {
-  composeWithDevTools
-} from "redux-devtools-extension";
-import users from "redux/modules/users";
-import {
-  i18nState
-} from "redux-i18n";
+import { composeWithDevTools } from "redux-devtools-extension";
+import user from "redux/modules/user";
+import { i18nState } from "redux-i18n";
 import Reactotron from "ReactotronConfig";
 
 const env = process.env.NODE_ENV;
@@ -24,14 +13,12 @@ const history = createHistory();
 const middlewares = [thunk, routerMiddleware(history)];
 
 if (env === "development") {
-  const {
-    logger
-  } = require("redux-logger");
+  const { logger } = require("redux-logger");
   middlewares.push(logger);
 }
 
 const reducer = combineReducers({
-  users,
+  user,
   routing: routerReducer,
   i18nState
 });
@@ -48,7 +35,5 @@ if (env === "development") {
   store = initialState => createStore(reducer, applyMiddleware(...middlewares));
 }
 
-export {
-  history
-};
+export { history };
 export default store();
