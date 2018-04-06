e801e4a4edb46bef0551e4f0f6d8b8b7649ef6a0
rdm-surface <vanadis0929@gmail.com>
2018-04-06 22:05:30 +0900

    3-52 완료

--- frontend/src/components/FeedPhoto/presenter.js
+++ frontend/src/components/FeedPhoto/presenter.js
@@ -4,7 +4,7 @@ import styles from "./styles.scss";
 import PhotoActions from "components/PhotoActions";
 import PhotoComments from "components/PhotoComments";
 import TimeStamp from "components/TimeStamp";
-//import CommentBox from "components/CommentBox";
+import CommentBox from "components/CommentBox";
 
 const FeedPhoto = (props, context) => {
   return (
@@ -28,6 +28,7 @@ const FeedPhoto = (props, context) => {
           comments={props.comments}
         />
         <TimeStamp time={props.natural_time} />
+        <CommentBox />
       </div>
     </div>
   );
