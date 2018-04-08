ca128adb244d26cd2f037c55f85c36481027e240
rdm-surface <vanadis0929@gmail.com>
2018-04-08 17:40:39 +0900

    3-60 완료 / css 수정

--- frontend/src/components/PhotoActions/presenter.js
+++ frontend/src/components/PhotoActions/presenter.js
@@ -21,10 +21,10 @@ const PhotoActions = (props, context) => (
         <Ionicon icon="ios-text-outline" fontSize="26px" color="black" />
       </button>
     </div>
-    <span className={styles.likes}>
-      {props.number}{" "}
+    <button type="button" className={styles.likes} onClick={props.openLikes}>
+      {props.number}
       {props.number === 1 ? context.t("like") : context.t("likes")}
-    </span>
+    </button>
   </div>
 );
 
@@ -32,7 +32,8 @@ PhotoActions.propTypes = {
   number: PropTypes.number.isRequired,
   isLiked: PropTypes.bool.isRequired,
   photoId: PropTypes.number.isRequired,
-  handleHeartClick: PropTypes.func.isRequired
+  handleHeartClick: PropTypes.func.isRequired,
+  openLikes: PropTypes.func.isRequired
 };
 
 PhotoActions.contextTypes = {
---
+++ frontend/src/components/UserList/presenter.js
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./styles.scss";
+import Loading from "components/Loading";
+import Ionicon from "react-ionicons";
+
+const UserList = props => (
+  <section className={styles.container}>
+    <div className={styles.overlay} />
+    <div className={styles.box}>
+      <header className={styles.header}>
+        <h4 className={styles.title}>{props.title}</h4>
+        <button type="button" onClick={props.closeLikes}>
+          <Ionicon icon="md-close" fontSize="20px" color="black" />
+        </button>
+      </header>
+      <div className={styles.content}>{props.loading ? <Loading /> : null}</div>
+    </div>
+  </section>
+);
+
+UserList.propTypes = {
+  title: PropTypes.string.isRequired,
+  loading: PropTypes.bool.isRequired,
+  users: PropTypes.array,
+  closeLikes: PropTypes.func.isRequired
+};
+
+export default UserList;
