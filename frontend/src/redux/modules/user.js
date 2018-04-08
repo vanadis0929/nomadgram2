55555a32d922721405d0f3a410465296a9879a24
rdm-surface <vanadis0929@gmail.com>
2018-04-08 18:19:53 +0900

    3-61 완료

--- frontend/src/redux/modules/user.js
+++ frontend/src/redux/modules/user.js
@@ -4,6 +4,7 @@
 
 const SAVE_TOKEN = "SAVE_TOKEN";
 const LOGOUT = "LOGOUT";
+const SET_USER_LIST = "SET_USER_LIST";
 
 // action creators
 
@@ -17,6 +18,13 @@ function saveToken(token) {
 function logout() {
   return {
     type: LOGOUT
+  };
+}
+
+function setUserList(userList) {
+  return {
+    type: SET_USER_LIST,
+    userList
   };
 }
 
@@ -90,6 +98,26 @@ function createAccount(username, password, email, name) {
   };
 }
 
+function getPhotoLikes(photoId) {
+  return (dispatch, getState) => {
+    const { user: { token } } = getState();
+    fetch(`/images/${photoId}/likes/`, {
+      headers: {
+        Authorization: `JWT ${token}`
+      }
+    })
+      .then(response => {
+        if (response.status === 401) {
+          dispatch(logout());
+        }
+        return response.json();
+      })
+      .then(json => {
+        dispatch(setUserList(json));
+      });
+  };
+}
+
 // initial state
 
 const initialState = {
@@ -103,6 +131,8 @@ function reducer(state = initialState, action) {
   switch (action.type) {
     case SAVE_TOKEN:
       return applySetToken(state, action);
+    case SET_USER_LIST:
+      return applySetUserList(state, action);
     case LOGOUT:
       return applyLogout(state, action);
     default:
@@ -129,13 +159,19 @@ function applyLogout(state, action) {
   };
 }
 
+function applySetUserList(state, action) {
+  const { userList } = action;
+  return { ...state, userList };
+}
+
 // exports
 
 const actionCreators = {
   facebookLogin,
   usernameLogin,
   createAccount,
-  logout
+  logout,
+  getPhotoLikes
 };
 
 export { actionCreators };
